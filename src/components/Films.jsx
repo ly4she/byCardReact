1eff28d388625a4911ad54c001a200432dbdc4cb
ly4she <ly4she@gmail.com>
2017-07-30 20:36:05 +0300

    films

--- src/components/Films.jsx
+++ src/components/Films.jsx
@@ -1,36 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-let films = '';
+export default class Films extends Component {
+	state = {
+		films: []
+	}
 
-function getFilms() {
-	
+	componentDidMount() {
+		this.getFilms();
+	}
+
+	getFilms = () => {
 	fetch('https://gist.githubusercontent.com/Kirit0s/53a9820793c1cdf22a9bfb1b71dbc84d/raw/14c5ee2ff6f9fef5e5dbd19b93c28ec45be32791/db')
 	.then(response => response.json())
 	.then(data => {
-		data.forEach((element) => {
-			let blockToAdd = `<div class="films__item">
-			<div class="films__date">
-			${element.dateStart}
+		const films = [];
+		data.forEach((element, i) => {
+			
+			let blockToAdd = <div key={i} className="films__item">
+			<div className="films__date">
+			{element.dateStart}
 			</div>
-			<div class="films__title">
-			${element.title}
+			<div className="films__title">
+			{element.title}
 			</div>
-			<img class="films__image" src="${element.image}" alt="">
-			<button class="films__subscribe">подписаться</button>
-			</div>`;
-			films = blockToAdd;
+			<img className="films__image" src={element.image} alt="" />
+			<button className="films__subscribe">подписаться</button>
+			</div>
+			films.push(blockToAdd);
 		});
-		
+		this.setState({
+				films
+			})
 	});
-	console.log(1);
 }
 
-export default function Films() {
-	//const fm = ;
-	console.log(films + "riba");
-	return(
+
+
+	
+	render (){
+		const { films } = this.state;
+		return(
 		<div className="films">
-			
+			{films}
 		</div>
-	)
+		);
+	}
 }
