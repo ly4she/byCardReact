5d62ce1501ae50613d41d47b1e4e2a53d48b0747
ly4she <ly4she@gmail.com>
2017-07-31 20:39:27 +0300

    vsCode

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 import Films from '../Films/Films.jsx';
-//import './Content.css';
 
 export default function Content() {
-	return(
-		<div className="content">
-		<div className="content__logo">
-			<h2>Узнай первым о появлении билетов в продаже</h2>
-		</div>
+  return(
+    <div className="content">
+    <div className="content__logo">
+      <h2>Узнай первым о появлении билетов в продаже</h2>
+    </div>
 
-		<Films />
-	</div>
-	)
+    <Films />
+  </div>
+  )
 }
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,31 +1,29 @@
 import React, {Component} from 'react';
 
 export default class Menu extends Component {
-	toggleMenu = () => {
-		var el = document.querySelector('.header__menuBtn');
+	toggleMenu = (event) => {
+		var el = event.target;
 		document.querySelector('.header__hiddenMenu').classList.toggle('header__menu--hideMenu');
 		document.addEventListener('click', outsideEvtListener);
-			function outsideEvtListener(evt) {
-				if (evt.target === el || el.contains(evt.target)) {
-					return;
-				} else document.querySelector('.header__hiddenMenu').classList.toggle('header__menu--hideMenu');
+		function outsideEvtListener(evt) {
+			if (evt.target === el || el.contains(evt.target)) {
+				return;
+			} else document.querySelector('.header__hiddenMenu').classList.toggle('header__menu--hideMenu');
 	    document.removeEventListener('click', outsideEvtListener);
 		}
 	}
 	render(){
-	return (
-		<div>
-			<div className="header__menu">
-					<div className="header__menuBtn" onClick={this.toggleMenu}>
+		return (
+			<div>
+				<div className="header__menu">
+					<div className="header__menuBtn" onClick={this.toggleMenu}></div>
 				</div>
-					
-				</div>
-			<ul className="header__hiddenMenu header__menu--hideMenu">
-				<li><a href="">Link 1</a></li>  
-				<li><a href="">Link 2</a></li>
-				<li><a href="">Link 3</a></li>  
-			</ul>
-		</div>
+				<ul className="header__hiddenMenu header__menu--hideMenu">
+					<li><a href="">Link 1</a></li>
+					<li><a href="">Link 2</a></li>
+					<li><a href="">Link 3</a></li>
+				</ul>
+			</div>
 		)
 	}
 }
