4bc70cfb0516e35dde64a7cd1ab931222d40caa0
ly4she <ly4she@gmail.com>
2017-07-31 20:00:10 +0300

    ideal menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,28 +3,16 @@ import React, {Component} from 'react';
 import Menu from '../Menu/Menu.jsx';
 
 export default class Header extends Component {
-	state = {
-		isHide: false
-	}
 	
-	toggleMenu = () => {
-		this.setState({
-			isHide: (!this.state.isHide)
-		})
-	}
 
 	render() {
 		return (
 			<header className="header">
-				<div className="header__menu">
-					<div className="header__menuBtn" onClick={this.toggleMenu}>
-					</div>
-					<Menu hideMenu={this.toggleMenu} isHide={this.state.isHide}/>
+				<Menu />
+				<div className="header__logo">
+					<h1>Bycard оповещения</h1>
 				</div>
-			<div className="header__logo">
-				<h1>Bycard оповещения</h1>
-			</div>
-		</header>
+			</header>
 		)
 	}
 }
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default function Menu (props) {
-	console.log(props);
-	return !props.isHide ? <div></div> :
+export default class Menu extends Component {
+	toggleMenu = () => {
+		var el = document.querySelector('.header__menuBtn');
+		document.querySelector('.header__hiddenMenu').classList.toggle('header__menu--hideMenu');
+		document.addEventListener('click', outsideEvtListener);
+			function outsideEvtListener(evt) {
+				if (evt.target === el || el.contains(evt.target)) {
+					return;
+				} else document.querySelector('.header__hiddenMenu').classList.toggle('header__menu--hideMenu');
+	    document.removeEventListener('click', outsideEvtListener);
+		}
+	}
+	render(){
+	return (
 		<div>
-			<ul className="header__hiddenMenu">
+			<div className="header__menu">
+					<div className="header__menuBtn" onClick={this.toggleMenu}>
+				</div>
+					
+				</div>
+			<ul className="header__hiddenMenu header__menu--hideMenu">
 				<li><a href="">Link 1</a></li>  
 				<li><a href="">Link 2</a></li>
 				<li><a href="">Link 3</a></li>  
 			</ul>
-			<div className="header__overlay" onClick={props.hideMenu}></div>
 		</div>
+		)
+	}
 }
