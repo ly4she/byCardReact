d3d47fd92be13d1fe5a6fd7a906ac581ae24b8ce
ly4she <ly4she@gmail.com>
2017-08-09 22:12:18 +0300

    svg menu ico

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -13,7 +13,10 @@ export default class Menu extends Component {
     return (
       <div className="header__menu">
         <Button raised className="header__menuBtn" onClick={()=> { this.setState({isOpen: !this.state.isOpen}) }}>
-          <i className="material-icons">&#xE5D2;</i>
+        <svg fill="#000000" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0h24v24H0z" fill="none"/>
+          <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+        </svg>
         </Button>
           <Drawer
             open={this.state.isOpen}
@@ -21,8 +24,11 @@ export default class Menu extends Component {
           >
             <DrawerHeader>
               <DrawerHeaderContent>
-              <Button raised className="header__hiddenMenuBtn" onClick={()=> { this.setState({isOpen: !this.state.isOpen}) }}>
-                <i className="material-icons">&#xE5CD;</i>
+                <Button raised className="header__hiddenMenuBtn" onClick={()=> { this.setState({isOpen: !this.state.isOpen}) }}>
+                <svg fill="#000000" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+                  <path d="M0 0h24v24H0z" fill="none"/>
+                </svg>
                 </Button>
                 Меню
               </DrawerHeaderContent>
