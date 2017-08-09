f483f0885f6c17b91043a7bced57604258c545c8
ly4she <ly4she@gmail.com>
2017-08-09 21:34:10 +0300

    data

--- src/components/Film/Film.jsx
+++ src/components/Film/Film.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
-
+import { Button } from 'react-mdc-web';
 export default function Film(props) {
 
   const {dateStart, title, image} = props.film;
 
-  let date = dateStart.match(/\d\d\d\d-\d\d-\d\d/);
+  let date = new Date(dateStart);
+  let dateToView = date.toLocaleString('ru', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+  })
+
 
 	return (
 		<div className="films__item">
 			<div className="films__date">
-				{date}
+				{dateToView}
 			</div>
 			<div className="films__title">
 				{title}
 			</div>
 			<img className="films__image" src={image} alt="" />
-			<button className="films__subscribe">подписаться</button>
+			<Button rased primary className="films__subscribe">подписаться</Button>
 		</div>
 		)
 }
